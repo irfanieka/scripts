05f09c41967f87d6aed264e87ed2e278c6f82f62
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-08-26 13:33:31 +0200

    .

--- install_sockd_and_enable_it_at_boot_time.sh
+++ install_sockd_and_enable_it_at_boot_time.sh
@@ -2,7 +2,7 @@
 apt update;apt -y install gcc cmake binutils build-essential net-tools
 ifaceName=$(ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}')
 wget https://www.inet.no/dante/files/dante-1.4.3.tar.gz
-tar -xvzf dante-1.4.3.tar.gz
+tar -xf dante-1.4.3.tar.gz
 cd dante-1.4.3
 ./configure
 make
@@ -38,34 +38,32 @@ EOF
 useradd --shell /usr/sbin/nologin mikrotik999
 echo "mikrotik999:Elibawnos" | chpasswd
 
-cat > /etc/rc.local <<END
-#!/bin/sh
-/usr/local/sbin/sockd -D &
-END
-chmod +x /etc/rc.local
+netstat -ntlp
 
-cat > /etc/systemd/system/rc-local.service <<EOL
+cat > /etc/systemd/system/startsockd.service <<EOL
 [Unit]
-Description=/etc/rc.local Compatibility
-ConditionPathExists=/etc/rc.local
+Description=start sockd at boot time
+After=network.target
+StartLimitIntervalSec=0
 
 [Service]
-Type=oneshot
-ExecStart=/etc/rc.local start
-TimeoutSec=0
-StandardOutput=tty
-RemainAfterExit=yes
-SysVStartPriority=99
+Type=simple
+Restart=always
+RestartSec=1
+User=root
+ExecStartPre=
+ExecStart=/bin/bash -c '/usr/local/sbin/sockd -D &'
+ExecStartPost
+ExecStop=
+ExecReload=
 
 [Install]
 WantedBy=multi-user.target
 EOL
 
-chmod +x /etc/systemd/system/rc-local.service
+systemctl daemon-reload
+systemctl enable startsockd.service
+systemctl start startsockd.service
+systemctl status startsockd.service
 
-systemctl enable rc-local
-systemctl start rc-local.service
-systemctl status rc-local.service
-
-netstat -ntlp
 
