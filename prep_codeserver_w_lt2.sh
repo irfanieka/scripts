5310777b8d9f91dc0ca6317c77c5b293422f8681
nathanfleight <92636939+nathanfleight@users.noreply.github.com>
2022-10-24 17:10:54 +0200

    Create prep_codeserver_w_lt2.sh

---
+++ prep_codeserver_w_lt2.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sudo -i
+apt update >/dev/null;apt -y install python3 python3-pip >/dev/null
+wget https://github.com/coder/code-server/releases/download/v4.4.0/code-server-4.4.0-linux-amd64.tar.gz
+tar -xf code-server-4.4.0-linux-amd64.tar.gz
+cd code-server-4.4.0-linux-amd64
+cd bin
+export PASSWORD="Elibawnos"
+./code-server --bind-addr 127.0.0.1:1234 & npx localtunnel --port 1234
